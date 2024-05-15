6db29fc72fe0173128aec0688fa1bcab9a6b96de
yagi-shi <yukisoccer.kk@gmail.com>
2024-05-16 01:46:16 +0900

    kadai7

---
+++ kadai7/ViewController2.swift
@@ -0,0 +1,32 @@
+//
+//  File.swift
+//  kadai7
+//
+//  Created by 八木佑樹 on 2024/05/15.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+    @IBOutlet weak var numField1: UITextField!
+    @IBOutlet weak var numField2: UITextField!
+    
+    @IBOutlet weak var subtractResult: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        subtractResult.text = ""
+        // テキストフィールドのキーボードタイプを数字専用に設定
+        numField1.keyboardType = .numberPad
+        numField2.keyboardType = .numberPad
+    }
+    
+    @IBAction func substractCalculate(_ sender: Any) {
+        let num1 = Int(numField1.text ?? "") ?? 0
+        let num2 = Int(numField2.text ?? "") ?? 0
+        let result = num1 - num2
+        subtractResult.text = String(result)
+        numField1.endEditing(true)
+        numField2.endEditing(true)
+    }
+}
